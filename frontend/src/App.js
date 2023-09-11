337810dcf1de5683759768850cab7d619d120efa
Isha Jain <63011793+ish-0709@users.noreply.github.com>
2023-09-11 15:23:00 +0530

    Updated auth.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,6 @@
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Login/>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-<<<<<<< Updated upstream
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Auth from './components/Auth';
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" element={<Auth />} />
-       
-      </Routes>
-    </BrowserRouter>
-  )
-=======
 import './App.css';
 import Auth from './components/Auth';
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -36,10 +8,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
     <div className="App">
-      <Auth/>
+      <Auth />
     </div>
   );
->>>>>>> Stashed changes
 }
 
 export default App
