be1c4f0a85f0228bb77b0a2bae11bdc993eaaa6c
unknown <sankitanitdgp28@gmail.com>
2023-09-14 17:21:50 +0530

    added txn table dummy data

--- frontend/src/components/TransactionHistory.js
+++ frontend/src/components/TransactionHistory.js
@@ -2,64 +2,53 @@
 // import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 // const TransactionHistory = () => {
-//     const [formData, setFormData] = useState({ 
+//     const [formData, setFormData] = useState({
 
 //     });
 
+// TransactionHistory.js
+import React, { Component, useEffect, useState } from "react";
+import "../styles/TransactionHistory.css";
+import txn from "../utils/txn";
 
-
-    // TransactionHistory.js
-import React, { Component } from 'react';
-
-class TransactionHistory extends Component {
-  state = {
-    transactions: [], // Store transaction data here
-  };
+function TransactionHistory() {
+  const [transactions, setTransactions] = useState(txn);
 
   // Fetch transaction data when the component mounts
-  componentDidMount() {
-    // Replace this with your actual API endpoint or data source
-    fetch('https://api.example.com/transactions')
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ transactions: data });
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-      });
-  }
+  // useEffect(()=>{
+  //   setTransactions()
+  // })
 
-  render() {
-    const { transactions } = this.state;
-
-    return (
-      <div>
-        <h2><center>Transaction History</center></h2>
-        <br></br>
-        <table className="table">
-          <thead>
+  return (
+    <div className="table-container">
+      <h2>
+        <center>Transaction History</center>
+      </h2>
+      <br></br>
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Txn ID</th>
+            <th>Sender's Acc No.</th>
+            <th>Receiver's Acc No.</th>
+            <th>Amount</th>
+            <th>Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
             <tr>
-              <th>Txn ID</th>
-              <th>Sender's Acc No.</th>
-              <th>Receiver's Acc No.</th>
-              <th>Amount</th>
-              <th>Timestamp</th>
+              <td>{transaction.id}</td>
+              <td>{transaction.senderAccNo}</td>
+              <td>{transaction.receiverAccNo}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.timestamp}</td>
             </tr>
-          </thead>
-          <tbody>
-            {transactions.map((transaction) => (
-              <tr key={transaction.id}>
-                <td>{transaction.senderAccNo}</td>
-                <td>{transaction.receiverAccNo}</td>
-                <td>{transaction.amount}</td>
-                <td>{transaction.timestamp}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default TransactionHistory;
