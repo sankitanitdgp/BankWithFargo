19fcafa768541a70f89adfdbd499a7a07275192e
unknown <sankitanitdgp28@gmail.com>
2023-09-15 15:06:31 +0530

    Dashboard modified create acc tab removed from nav

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -17,16 +17,19 @@ function Dashboard() {
     <>
       <div>
         <Link to="/transactions">
-          <Card
-            className="dashboard-card card-img-"
-            onClick={handleOnClickTransactions}
-          >
+          <Card className="dashboard-card" onClick={handleOnClickTransactions}>
             <Card.Body>View Transactions</Card.Body>
           </Card>
         </Link>
         <Card className="dashboard-card" onClick={handleOnClickBalance}>
           <Card.Body>Check Balance</Card.Body>
         </Card>
+        <Link to="/openAccount">
+          <Card className="dashboard-card" onClick={handleOnClickTransactions}>
+            <Card.Body>Open a Savings Accounts</Card.Body>
+          </Card>
+        </Link>
+
         {show && <ViewBalanceModal show={show} setShow={setShow} />}
       </div>
     </>
