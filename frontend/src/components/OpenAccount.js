5a66e396782711b8536f7c098a4b26f5a9e71416
manan132000 <74011889+manan132000@users.noreply.github.com>
2023-09-15 15:03:52 +0530

    Update OpenAccount.js

--- frontend/src/components/OpenAccount.js
+++ frontend/src/components/OpenAccount.js
@@ -105,13 +105,9 @@ const SignUp = () => {
     } else {
       // Submit your data to the server or perform further actions here
       console.log(formData);
-<<<<<<< HEAD
       OpenAccountService.openAccount(formData).then((res) => console.log(res));
       setSuccess("visible");
-=======
-      OpenAccountService.openAccount
-      (formData).then(res=>console.log(res))
->>>>>>> 4e06b9ea987ac84e08bd3d078b216c673bc92b32
+
     }
   };
 
